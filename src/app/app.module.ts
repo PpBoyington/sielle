6d59e5953f1859aa6db7c08b92d59ad1dd9d7c95
Gregory <gregory.delfrate@gmail.com>
2018-11-01 23:49:10 +0100

    Create backbone for application npm installed all dep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,59 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule, MatCheckboxModule, MatDialogModule, MatMenuModule} from '@angular/material';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NewsComponent } from './news/news.component';
+import { AboutComponent } from './about/about.component';
+import { WorksComponent } from './works/works.component';
+import { TourComponent } from './tour/tour.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+
+
+const appRoutes: Routes = [
+  { path: 'news', component: NewsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'works', component: WorksComponent },
+  { path: 'tour', component: TourComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '',
+    redirectTo: '/news',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    NewsComponent,
+    AboutComponent,
+    WorksComponent,
+    TourComponent,
+    ContactComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatDialogModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
